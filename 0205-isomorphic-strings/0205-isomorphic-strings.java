7c6ba3937c999a986db6ca2ac61bf2768c46222c
Chinmaya Pradhan <111198586+PradhanChinmay@users.noreply.github.com>
2023-07-13 00:04:53 +0530

    Time: 11 ms (60.20%), Space: 41.2 MB (95.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,22 +3,27 @@ class Solution {
         
         if (s.length() != t.length()) return false;
         
-        HashMap<Character, Character> hm = new HashMap<>();
+        HashMap<Character, Character> hm1 = new HashMap<>();
+        HashMap<Character, Character> hm2 = new HashMap<>();
         
         for (int i = 0; i <= s.length() - 1; i++) {
             
-            if (hm.containsKey(s.charAt(i))) {
+            if (hm1.containsKey(s.charAt(i))) {
                 
-                if (t.charAt(i) != hm.get(s.charAt(i))) return false;
+                if (t.charAt(i) != hm1.get(s.charAt(i))) return false;
                 else continue;
                 
             }
-            else {
+            
+            if (hm2.containsKey(t.charAt(i))) {
                 
-                if (hm.containsValue(t.charAt(i))) return false;
+                if (s.charAt(i) != hm2.get(t.charAt(i))) return false;
+                else continue;
                 
-                hm.put(s.charAt(i), t.charAt(i));
             }
+            
+            if (!hm1.containsKey(s.charAt(i))) hm1.put(s.charAt(i), t.charAt(i));
+            if (!hm2.containsKey(t.charAt(i))) hm2.put(t.charAt(i), s.charAt(i));
             
         }
         
